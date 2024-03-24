fdc925f498976a0b96efd708077d2ef0cbfef1ae
Mahmudur Rashid <black43295@protonmail.com>
2024-03-24 21:16:14 +0600

    Minor changes

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -1,5 +1,9 @@
 const Scroll = (props) => {
-  return <div className="tc">{props.children}</div>;
+  return (
+    <div className="tc" style={{ overflowY: "scroll", height: "800px" }}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Scroll;
